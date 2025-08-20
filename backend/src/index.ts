6e43ebe691ea78ea94c9ac36e55f73ea60e60954
JeremyWarui <jerywarne7@gmail.com>
2025-08-20 22:19:14 +0300

    Define the schema type and the Queries

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,8 +2,44 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer} from "@apollo/server/standalone";
 
 const typeDefs = `
+    type User {
+      id: ID!
+      name: String!
+      email: String!
+    }
+    
+    enum IssueStatus {
+      OPEN
+      ASSIGNED
+      IN_PROGRESS
+      RESOLVED
+      CLOSED
+    }
+    
+    type Issue {
+      id: ID!
+      title: String!
+      description: String!
+      status: IssueStatus!
+      createdAt: String!
+      updatedAt: String!
+      assignedTo: User
+    }
+    
+    type Comment {
+      id: ID!
+      content: String!
+      author: User!
+      issue: Issue!
+      createdAt: String!
+    }
+    
     type Query {
       dummy: String!
+      issuesCount: Int!
+      issues(status: String, assignedTo: String): [Issue!]!
+      issue(id: ID!): Issue!
+      users: [User!]!
     }
 `
 
