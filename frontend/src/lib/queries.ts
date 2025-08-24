62a535413a2fa243dc2a2bc8b921d705b89800e4
JeremyWarui <jerywarne7@gmail.com>
2025-08-24 20:58:33 +0300

    Refactored the queries and mutations by using fragments to avoid repetition

--- frontend/src/lib/queries.ts
+++ frontend/src/lib/queries.ts
@@ -1,110 +1,120 @@
 import { gql } from "@apollo/client";
 
+
+const USER_INFO = gql`
+    fragment UserInfo on User {
+        id
+        name
+        email
+    }
+`
+
+const ISSUE_SUMMARY = gql`
+    fragment IssueSummary on Issue {
+        id
+        title
+        description
+        status
+        createdAt
+        updatedAt
+        assignedTo {
+            ...UserInfo
+        }
+        comments {
+            id
+        }
+    }
+    ${USER_INFO}
+`
+
+const ISSUE_DETAIL = gql`
+    fragment IssueDetails on Issue {
+        id
+        title
+        description
+        status
+        createdAt
+        updatedAt
+        assignedTo {
+            ...UserInfo
+        }
+        comments {
+            id
+            content
+            createdAt
+            author {
+                ...UserInfo
+            }
+        }
+    }
+    ${USER_INFO}
+`
+
 export const ALL_ISSUES_AND_USERS = gql`
     query allIssuesAndUsers($status: String, $assignedTo: String) {
         issues(status: $status, assignedTo: $assignedTo) {
-            id
-            title
-            description
-            status
-            updatedAt
-            assignedTo {
-                id
-                name
-            }
-            comments {
-                id
-            }
+            ...IssueSummary
         }
         users {
-            id
-            name
+            ...UserInfo
         }
     }
+    ${ISSUE_SUMMARY}
+    ${USER_INFO}
 `
 
 export const GET_ISSUE = gql(`
     query getIssue($id: ID!) {
         issue(id: $id) {
-            id
-            title
-            description
-            status
-            createdAt
-            updatedAt
-            assignedTo {
-                id
-                name
-            }
-            comments {
-                id
-                content
-                createdAt
-                author {
-                    id
-                    name
-                }
-            }
+            ...IssueDetails
         }
         users {
-            id
-            name
+            ...UserInfo
         }
     }
+    ${ISSUE_DETAIL}
+    ${USER_INFO}
 `)
 
 export const ALL_USERS = gql`
     query allUsers {
         users {
-            id
-            name
-            email
+            ...UserInfo
             assignedIssues {
                 id
             }
         }
     }
+    ${USER_INFO}
 
 `
 
 export const CREATE_ISSUE = gql(`
     mutation createIssue($title: String!, $description: String!) {
         createIssue (title: $title, description: $description) {
-            id
-            title
-            description
-            status
-            createdAt
-            updatedAt
+            ...IssueSummary
         }
     }
+    ${ISSUE_SUMMARY}
 `)
 
-export const ASSIGN_ISSUE = gql( `
+export const ASSIGN_ISSUE = gql(`
     mutation assignIssue($id: ID!, $userId: String!) {
         assignIssue(id: $id, userId: $userId) {
-            id
-            title
-            description
-            status
-            assignedTo {
-                name
-            }
-            updatedAt
+            ...IssueSummary
         }
     }
+    ${ISSUE_SUMMARY}
   `
 )
 
 export const UPDATE_ISSUE_STATUS = gql`
     mutation updateIssueStatus($id: ID!, $status: IssueStatus!) {
         updateIssueStatus(id: $id, status: $status) {
-            id
-            title
-            description
-            status
+            ...IssueSummary
         }
     }
+    ${ISSUE_SUMMARY}
 `
 
 export const CREATE_COMMENT = gql`
@@ -113,10 +123,37 @@ export const CREATE_COMMENT = gql`
             id
             content
             author {
+                ...UserInfo
+            }
+            createdAt
+        }
+    }
+    ${USER_INFO}
+`
+
+
+export const GET_ISSUES_SUMMARY = gql`
+    query getIssuesSummary {
+        issues {
+            id
+            title
+            status
+            updatedAt
+            assignedTo {
                 id
                 name
             }
-            createdAt
+        }
+    }
+`
+
+export const GET_USER_ASSIGNMENTS = gql`
+    query getUserAssignments($userId: String!) {
+        issues(assignedTo: $userId) {
+            id
+            title
+            status
+            updatedAt
         }
     }
 `
