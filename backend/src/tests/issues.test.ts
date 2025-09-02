1d5800e51c736d0fd1803a6df8b4cb08fb0bc1b4
JeremyWarui <jerywarne7@gmail.com>
2025-09-02 21:53:09 +0300

    Test the issues resolver returns the comments array and assignedTo fields

--- backend/src/tests/issues.test.ts
+++ backend/src/tests/issues.test.ts
@@ -6,6 +6,8 @@ import { resolvers } from "../graphql/resolvers.ts"
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
 import { generateTestData } from "./helpers/test_data.ts"
+import { assertObjectType } from "graphql/type/index.js";
+import { User } from "../models/users.ts";
 
 process.env.NODE_ENV = "test";
 
@@ -48,6 +50,45 @@ describe("GraphQL issues API", () => {
         expect(response.status).toBe(200)
         expect(response.body.data?.issues).toBeInstanceOf(Array);
         expect(response.body.data.issues.length).toBeGreaterThan(0);
+    })
+
+    it("fetches issues with comments array", async () => {
+        const query = `{
+            issues {
+                id
+                assignedTo {
+                    id
+                }
+                comments {
+                    id
+                }
+            }
+        }
+        `
+        const response = await request(url).post("/").send({ query });
+
+        expect(response.status).toBe(200);
+        expect(response.body.data?.issues[0]?.comments).toBeInstanceOf(Array)
+        expect(response.body.data?.issues[0]?.comments.length).toBeGreaterThanOrEqual(0)
+    })
+
+
+    it("fetches issues with assignedTo of first issue to be null", async () => {
+        const query = `{
+            issues {
+                id
+                assignedTo {
+                    id
+                }
+            }
+        }
+        `
+        const response3 = await request(url).post("/").send({ query });
+
+        expect(response3.status).toBe(200);
+        // expect(Object.keys(response3.body.data?.issues[0]?.assignedTo)).toContain("id")
+        expect(response3.body.data?.issues[0]?.assignedTo).toBeDefined()
+        expect(response3.body.data?.issues[0]?.assignedTo).toEqual(null)
     })
 
 })
